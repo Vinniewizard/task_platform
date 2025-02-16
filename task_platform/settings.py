3dc3222d7c87d01fc54deeb3ead9e237a198f05a
Vincent Kipkoech <wizard@Kali.kali.com>
2025-02-16 03:51:51 +0300

    Added CSRF_TRUSTED_ORIGINS for Railway

--- task_platform/settings.py
+++ task_platform/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-m(5r$+0yt376vz67ift0*g!z%z!zd@4q$ven7*vc(hc^r-2s9v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["web-production-cc86f.up.railway.app", "127.0.0.1", "localhost"]
+CSRF_TRUSTED_ORIGINS = ["https://web-production-cc86f.up.railway.app"]
 
 
 # Application definition
