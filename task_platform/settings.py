236687bfb1c224501fb3f011bfb61cfa7f3fa409
Vinniewizard <164242787+Vinniewizard@users.noreply.github.com>
2025-02-19 18:31:31 +0300

    Update settings.py

--- task_platform/settings.py
+++ task_platform/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m(5r$+0yt376vz67ift0*g!z%z!zd@4q$ven7*vc(hc^r-2s9v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "task-platform-1-3d0l.onrender.com"
