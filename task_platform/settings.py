8fc032469b9ccd006e5281dba8de28955ce8ea9b
Vincent Kipkoech <wizard@Kali.kali.com>
2025-02-26 02:47:00 +0300

    Fix database and ALLOWED_HOSTS

--- task_platform/settings.py
+++ task_platform/settings.py
@@ -93,19 +93,24 @@ TEMPLATES = [
 WSGI_APPLICATION = 'task_platform.wsgi.application'
 
 # settings.py
-DATABASE_URL = os.getenv("DATABASE_URL")
+import os
+import dj_database_url
+
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 if DATABASE_URL:
     DATABASES = {
         'default': dj_database_url.config(default=DATABASE_URL)
     }
 else:
+    # Default to SQLite for local development
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
+
 
 SESSION_ENGINE = "django.contrib.sessions.backends.db"
 SESSION_COOKIE_AGE = 1209600  # 2 weeks
