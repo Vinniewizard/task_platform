762759ef908ca211dd8554203576dd1c251fc067
Vincent Kipkoech <wizard@Kali.kali.com>
2025-02-26 02:11:01 +0300

    Initial commit

--- task_platform/settings.py
+++ task_platform/settings.py
@@ -23,11 +23,7 @@ SECRET_KEY = 'django-insecure-m(5r$+0yt376vz67ift0*g!z%z!zd@4q$ven7*vc(hc^r-2s9v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "web-production-cc86f.up.railway.app",
-    "localhost",
-    "127.0.0.1"
-]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
 
 
 CSRF_TRUSTED_ORIGINS = [
