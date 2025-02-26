85d758117994822dbb3eab7e3d414968d895d03f
Vincent Kipkoech <wizard@Kali.kali.com>
2025-02-27 01:08:03 +0300

    Updated project settings and templates

--- task_platform/settings.py
+++ task_platform/settings.py
@@ -99,12 +99,37 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+import os
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'task_platform_db',  # Local MySQL database name
+        'USER': 'vinnie',  # Local MySQL user
+        'PASSWORD': '11',  # Local MySQL password
+        'HOST': '127.0.0.1',  # Localhost
+        'PORT': '3306',  # Default MySQL port
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+    },
+    'railway': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': 'naVGayhUkfshQKProdpFojMyitSgAZSD',
+        'HOST': 'switchyard.proxy.rlwy.net',  # Public Railway Host
+        'PORT': '43932',  # Public Railway Port
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
+
+# Switch to Railway database if environment variable is set
+if os.getenv('USE_RAILWAY_DB', 'False') == 'True':
+    DATABASES['default'] = DATABASES['railway']
+
 
 
 SESSION_ENGINE = "django.contrib.sessions.backends.db"
